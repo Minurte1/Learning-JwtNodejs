03adf46e11647c86d3334006fe16012231397c01
Minurte1 <hohoangphucjob@gmail.com>
2024-02-15 12:18:59 +0700

    step 2 Hellowork wih Nodejs Ẽpress and setup nodejs

--- src/Server.js
+++ src/Server.js
@@ -1,13 +1,13 @@
 import express from "express"
 import configViewEndine from "./config/viewEnigne"
 import initWebRouter from "./router/web"
-
+require('dotenv').config();
 const app = express();
 
 //config view engine
 configViewEndine(app);
 initWebRouter(app);
-const PORT = 8080;
+const PORT = process.env || 8080;
 app.listen(PORT, () => {
     console.log(">> JWT back stating = " + PORT);
 })
