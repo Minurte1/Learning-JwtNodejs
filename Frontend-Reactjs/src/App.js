690566f91ccd3236a6fdf30fdfce59f0d15810e1
Minurte1 <hohoangphucjob@gmail.com>
2024-03-07 20:34:13 +0700

    cap nhat giao dien Login fb

--- Frontend-Reactjs/src/App.js
+++ Frontend-Reactjs/src/App.js
@@ -1,50 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
-import Nav from "./components/Nav";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   console.log('check')
   return (
-    <>
-      <Router>
-        <Nav></Nav>
-        <div className="app-container">
-
-          <Switch>
-            <Route path="/">
-              home
-            </Route>
-
-            <Route path="/about">
-              about
-            </Route>
-
-            <Route path="/login">
-              <Login></Login>
-              gê
-            </Route>
-
-            <Route path="/news">
-              newss
-            </Route>
-
-            <Route path="/contact">
-              contacts
-            </Route>
-
-            <Route path="*">
-              404 Not Found
-            </Route>
-          </Switch>
-        </div>
-      </Router >
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        {/* Các Route khác nếu cần */}
+      </Switch>
+    </Router>
   );
 }
 
--- Frontend-Reactjs/src/components/Login/Login.js
+++ Frontend-Reactjs/src/components/Login/Login.js
@@ -1,14 +1,68 @@
+import React from 'react';
+import '../Login/Login.scss'; // Import CSS file
+import LogoFb from './image/logo.svg'
+const LoginForm = () => {
+    return (
+        <div className="wrap">
+            <div className="container-fb">
+                <div className="fb-thongtin">
+                    <img src={LogoFb} className="fb" style={{ height: "100px", width: "300px" }} alt="Facebook logo" />
+                    <p>Facebook helps you connect and share with the people in your life.</p>
+                </div>
+                <div className="form">
+                    <div className="form-login">
+                        <form>
+                            <input type="text" placeholder="Email address or phone number" className="input1" />
+                            <input type="password" placeholder="Password" className="input2" />
+                            <button type="submit" className="button">Log in</button>
+                            <a href="">Forgotten password?</a>
+                            <div className="thanhnang"></div>
+                            <button type="submit" className="button1">Create new account</button>
+                        </form>
+                    </div>
+                    <a href="/" className="create"> Create a Page</a> <span>for a celebrity, brand or business.</span>
+                </div>
+            </div>
+        </div>
+    );
+}
 
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footerall">
+                <div className="footer-a">
+                    <a href="">English (UK)</a>
+                    <a href="">Tiếng Việt</a>
+                    <a href="">中文(台灣)</a>
+                    <a href="">한국어</a>
+                    <a href="">日本語</a>
+                    <a href="">Français (France)</a>
+                    <a href="">ภาษาไทย</a>
+                    <a href="">Español</a>
+                    <a href="">Português (Brasil)</a>
+                    <a href="">Deutsch</a>
+                    <a href="">Italiano</a>
+                </div>
+                <div className="footer-hr"></div>
+                <div className="footer-a">
+                    {/* Your language links */}
+                </div>
+                <div className="footer-a">
+                    <a href="">Meta © 2024</a>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-
-const Login = (props) => {
+function Login() {
     return (
         <>
-            <div className="container-Login">
-                <button className="btn btn-primary">asdasd</button>
-            </div>
-
+            <LoginForm />
+            <Footer />
         </>
-    )
+    );
 }
-export default Login
+
+export default Login;
