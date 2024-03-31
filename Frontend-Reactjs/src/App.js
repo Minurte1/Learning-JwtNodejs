3ef11ba9d205a69852e4c9f8d6c854097f2c36a9
Minurte1 <hohoangphucjob@gmail.com>
2024-03-31 08:21:45 +0700

    backup p2

--- Frontend-Reactjs/src/App.js
+++ Frontend-Reactjs/src/App.js
@@ -15,18 +15,19 @@ function App() {
     if (session) {
       setAccount(JSON.parse(session));
     }
-  });
+  }, []);
   return (
     <Router>
-      {account && !_.isElement(account) && account.isAuthenticated && <Nav />}
-      <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/users" element={<Users />} />
-        {/* Các Route khác nếu cần */}
-      </Routes>
-
-      <ToastContainer position="top-left" autoClose={3000} draggable />
+      <div className="as">
+        {/* {account && !_.isElement(account) && account.isAuthenticated && <Nav />} */}
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/users" element={<Users />} />
+          {/* Các Route khác nếu cần */}
+        </Routes>
+        <ToastContainer position="top-left" autoClose={3000} draggable />{' '}
+      </div>
     </Router>
   );
 }
